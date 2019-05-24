bd730a6ddacebad1d91155110cc79fa84e576815
eyekorea <eyekorea0@gmail.com>
2019-05-24 20:53:04 +0900

    works.

---
+++ src/index.js
@@ -0,0 +1,32 @@
+/**
+ * @description sequential-class-name
+ */
+const o = {
+    element: document.body,
+    classList: ['ready', 'active'],
+    delayTime: 100,
+    add: false,
+    callback: null
+}
+
+class seqElement {
+    constructor(selector) {
+        this.elements = document.querySelectorAll(selector);
+    }
+    addClass(delayTime, ...classList) {
+
+    }
+    removeClass(delayTime, ...classList) {
+
+    }
+   
+}
+
+ function seqClass(selector = "body") {
+    var element = new seqElement(selector);
+    return element;
+}
+
+// var n = seqClass('body');
+
+export default seqClass;
